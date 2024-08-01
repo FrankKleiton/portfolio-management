479cbb010c2325a3a19535e91cd6af821e40ea38
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-08-01 14:26:12 -0300

    refactor: clean up test

--- tests/usecases/stock-summaries/StockSummariesUseCase.spec.ts
+++ tests/usecases/stock-summaries/StockSummariesUseCase.spec.ts
@@ -101,31 +101,32 @@ describe("StockSummariesUseCase", () => {
   describe("period", () => {
     let twentyOne: Period;
     let twentyTwo: Period;
+    let twentyOneToTwentyFour: Period;
 
     beforeAll(() => {
       twentyOne = new Period(2021);
       twentyTwo = new Period(2022);
+      twentyOneToTwentyFour = new Period(2021, 2024);
     });
 
     test("year equality", () => {
       expect(twentyOne.equals(twentyOne)).toBeTruthy();
       expect(twentyOne.equals(twentyOne)).toBeTruthy();
       expect(twentyOne.equals(twentyTwo)).toBeFalsy();
-      expect(new Period(2021, 2022).equals(new Period(2021))).toBeFalsy();
-      expect(
-        new Period(2021, 2022).equals(new Period(2021, 2022))
-      ).toBeTruthy();
+      expect(twentyOneToTwentyFour.equals(twentyOne)).toBeFalsy();
+      expect(twentyOneToTwentyFour.equals(twentyOneToTwentyFour)).toBeTruthy();
     });
 
     test("year greater than", () => {
       expect(twentyOne.greaterThen(twentyTwo)).toBeFalsy();
       expect(
-        new Period(2021, 2024).greaterThen(new Period(2021, 2021))
+        twentyOneToTwentyFour.greaterThen(new Period(2021, 2021))
       ).toBeTruthy();
 
       expect(
-        new Period(2021, 2024).greaterThen(new Period(2021, 2025))
+        twentyOneToTwentyFour.greaterThen(new Period(2021, 2025))
       ).toBeFalsy();
+      expect(twentyOneToTwentyFour.greaterThen(twentyOne)).toBeFalsy();
     });
   });
 });
