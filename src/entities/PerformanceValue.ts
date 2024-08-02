8029c4b04fdbec040fc8e54757e116e865dad0fb
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-08-02 09:27:36 -0300

    cleanup: remove dirty code

--- src/entities/PerformanceValue.ts
+++ src/entities/PerformanceValue.ts
@@ -1,17 +1,5 @@
 import { Period } from "./Period";
-class NumericParser {
-  parse(value: string) {
-    const amounts: { [key: string]: number } = {
-      M: 1000000,
-      B: 1000000000,
-    };
-    const suffix = value.at(-1) || "M";
-    return (
-      Number.parseFloat(value.replace(` ${suffix}`, "").replace(/\,/g, ".")) *
-      +amounts[suffix]
-    );
-  }
-}
+
 export class PerformanceValue {
   constructor(public value: number, public period: Period) {}
 
